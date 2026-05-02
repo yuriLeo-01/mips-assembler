322749b11638933e2dd24ac37befaaa3cd5679b9
Yuri <yurileo2005@gmail.com>
2026-05-02 15:45:19 -0300

    test: atualiza arquivos de teste (asm) e tabela de ciclos (csv)

--- teste.asm
+++ teste.asm
@@ -1,12 +1,4 @@
-# Exemplo
-main:
-    addi $t0, $zero, 10    # Tipo I: Inicializa contador
-    addi $t1, $zero, 0     # Tipo I: Inicializa soma
-LOOP:
-    add $t1, $t1, $t0      # Tipo R: soma = soma + contador
-    addi $t0, $t0, -1      # Tipo I: decrementa contador
-    bne $t0, $zero, LOOP   # Tipo I: salto condicional (se suportado)
-    sw $t1, 0($sp)         # Tipo I: salva na memória
-    j FIM                  # Tipo J: salto absoluto
-FIM:
-    jr $ra                 # Tipo R especial: retorno
+L1: add $t0, $s1, $s2
+L2: addi $t1, $s3, 7
+beq $t0, $t1, L1
+j L2
